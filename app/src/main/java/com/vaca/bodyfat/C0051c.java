56738610af0a9ca304a7f57889a1d164d096e32f
liguanghao <liguanghao@viatomtech.com>
2022-12-14 17:35:49 +0800

    fgdhtfghdfgdfgdr

--- app/src/main/java/com/vaca/bodyfat/C0051c.java
+++ app/src/main/java/com/vaca/bodyfat/C0051c.java
@@ -3,67 +3,67 @@ import com.holtek.libHTBodyfat.HTBodyResultTwoLegs;
 
 public class C0051c  {
 
-     /* renamed from: b */
+
     public double f678b;
 
-    /* renamed from: c */
+
     public double f679c;
 
-    /* renamed from: d */
+
     public int f680d;
 
-    /* renamed from: e */
+
     public double f681e;
 
-    /* renamed from: f */
+
     public double f682f;
 
-    /* renamed from: g */
+
     public double f683g;
 
-    /* renamed from: k */
+
     private boolean f687k;
 
-    /* renamed from: l */
+
     private boolean f688l;
 
-    /* renamed from: m */
+
     private boolean f689m;
 
-    /* renamed from: o */
+
     public ScaleInfo f691o;
 
-    /* renamed from: h */
+
     private boolean f684h = false;
 
-    /* renamed from: i */
+
     private boolean f685i = false;
 
-    /* renamed from: j */
+
     private boolean f686j = false;
 
-    /* renamed from: n */
+
     private boolean f690n = false;
 
-    /* renamed from: p */
+
     private double[][] f692p = {new double[]{10.2d, 11.1d, 14.8d, 16.3d, 18.1d}, new double[]{11.3d, 12.4d, 16.3d, 17.8d, 19.4d}, new double[]{12.5d, 13.7d, 17.8d, 19.4d, 21.1d}, new double[]{13.1d, 14.3d, 18.4d, 20.0d, 21.8d}, new double[]{13.4d, 14.5d, 18.7d, 20.3d, 22.1d}, new double[]{13.5d, 14.7d, 18.8d, 20.5d, 22.3d}, new double[]{13.6d, 14.7d, 18.8d, 20.5d, 22.3d}, new double[]{13.7d, 14.8d, 18.8d, 20.5d, 22.3d}, new double[]{13.6d, 14.7d, 18.7d, 20.4d, 22.2d}, new double[]{13.6d, 14.7d, 18.6d, 20.3d, 22.1d}, new double[]{13.5d, 14.6d, 18.5d, 20.1d, 22.0d}, new double[]{13.4d, 14.5d, 18.4d, 20.0d, 21.8d}, new double[]{13.4d, 14.4d, 18.2d, 19.8d, 21.6d}, new double[]{13.3d, 14.3d, 18.1d, 19.7d, 21.5d}, new double[]{13.2d, 14.2d, 18.0d, 19.6d, 21.3d}, new double[]{13.1d, 14.1d, 17.8d, 19.4d, 21.2d}, new double[]{13.1d, 14.0d, 17.7d, 19.3d, 21.0d}, new double[]{13.0d, 13.9d, 17.6d, 19.1d, 20.9d}, new double[]{12.9d, 13.9d, 17.5d, 19.0d, 20.8d}, new double[]{12.9d, 13.8d, 17.4d, 18.9d, 20.7d}, new double[]{12.8d, 13.7d, 17.3d, 18.8d, 20.6d}, new double[]{12.8d, 13.7d, 17.2d, 18.7d, 20.5d}, new double[]{12.7d, 13.6d, 17.2d, 18.7d, 20.4d}, new double[]{12.7d, 13.6d, 17.1d, 18.6d, 20.3d}, new double[]{12.7d, 13.6d, 17.0d, 18.5d, 20.3d}, new double[]{12.8d, 13.8d, 17.3d, 18.8d, 20.5d}, new double[]{12.8d, 13.7d, 17.3d, 18.8d, 20.5d}, new double[]{12.7d, 13.7d, 17.2d, 18.7d, 20.4d}, new double[]{12.7d, 13.6d, 17.2d, 18.7d, 20.4d}, new double[]{12.7d, 13.6d, 17.1d, 18.6d, 20.3d}, new double[]{12.6d, 13.6d, 17.1d, 18.6d, 20.2d}, new double[]{12.6d, 13.5d, 17.1d, 18.5d, 20.2d}, new double[]{12.5d, 13.5d, 17.0d, 18.5d, 20.1d}, new double[]{12.5d, 13.5d, 17.0d, 18.5d, 20.1d}, new double[]{12.5d, 13.4d, 17.0d, 18.4d, 20.0d}, new double[]{12.4d, 13.4d, 16.9d, 18.4d, 20.0d}, new double[]{12.4d, 13.4d, 16.9d, 18.4d, 20.0d}, new double[]{12.4d, 13.3d, 16.9d, 18.3d, 19.9d}, new double[]{12.3d, 13.3d, 16.8d, 18.3d, 19.9d}, new double[]{12.3d, 13.3d, 16.8d, 18.3d, 19.9d}, new double[]{12.3d, 13.2d, 16.8d, 18.2d, 19.9d}, new double[]{12.2d, 13.2d, 16.8d, 18.2d, 19.9d}, new double[]{12.2d, 13.2d, 16.8d, 18.2d, 19.8d}};
 
-    /* renamed from: q */
+
     private double[][] f693q = {new double[]{10.1d, 11.1d, 14.6d, 16.1d, 17.7d}, new double[]{10.8d, 12.0d, 16.0d, 17.5d, 19.1d}, new double[]{11.8d, 13.0d, 17.3d, 19.0d, 20.7d}, new double[]{12.4d, 13.6d, 17.9d, 19.7d, 21.5d}, new double[]{12.7d, 13.9d, 18.3d, 20.0d, 22.0d}, new double[]{12.9d, 14.1d, 18.4d, 20.2d, 22.2d}, new double[]{13.0d, 14.1d, 18.5d, 20.3d, 22.3d}, new double[]{13.0d, 14.2d, 18.5d, 20.3d, 22.3d}, new double[]{13.0d, 14.1d, 18.4d, 20.2d, 22.2d}, new double[]{12.9d, 14.1d, 18.3d, 20.1d, 22.1d}, new double[]{12.9d, 14.0d, 18.2d, 19.9d, 21.9d}, new double[]{12.8d, 13.9d, 18.0d, 19.8d, 21.8d}, new double[]{12.7d, 13.8d, 17.9d, 19.6d, 21.6d}, new double[]{12.6d, 13.7d, 17.7d, 19.5d, 21.4d}, new double[]{12.6d, 13.6d, 17.6d, 19.3d, 21.3d}, new double[]{12.5d, 13.5d, 17.5d, 19.2d, 21.1d}, new double[]{12.4d, 13.5d, 17.4d, 19.1d, 21.0d}, new double[]{12.4d, 13.4d, 17.3d, 18.9d, 20.9d}, new double[]{12.3d, 13.3d, 17.2d, 18.8d, 20.8d}, new double[]{12.3d, 13.3d, 17.1d, 18.8d, 20.7d}, new double[]{12.2d, 13.2d, 17.0d, 18.7d, 20.6d}, new double[]{12.2d, 13.2d, 17.0d, 18.6d, 20.5d}, new double[]{12.2d, 13.1d, 16.9d, 18.5d, 20.4d}, new double[]{12.2d, 13.1d, 16.9d, 18.5d, 20.4d}, new double[]{12.1d, 13.1d, 16.8d, 18.4d, 20.3d}, new double[]{12.4d, 13.3d, 17.1d, 18.7d, 20.6d}, new double[]{12.3d, 13.3d, 17.0d, 18.7d, 20.6d}, new double[]{12.3d, 13.3d, 17.0d, 18.6d, 20.5d}, new double[]{12.3d, 13.3d, 17.0d, 18.6d, 20.5d}, new double[]{12.3d, 13.2d, 17.0d, 18.6d, 20.4d}, new double[]{12.3d, 13.2d, 16.9d, 18.5d, 20.4d}, new double[]{12.2d, 13.2d, 16.9d, 18.5d, 20.4d}, new double[]{12.2d, 13.2d, 16.9d, 18.5d, 20.4d}, new double[]{12.2d, 13.1d, 16.9d, 18.5d, 20.3d}, new double[]{12.2d, 13.1d, 16.8d, 18.5d, 20.3d}, new double[]{12.1d, 13.1d, 16.8d, 18.4d, 20.3d}, new double[]{12.1d, 13.1d, 16.8d, 18.4d, 20.3d}, new double[]{12.1d, 13.1d, 16.8d, 18.4d, 20.3d}, new double[]{12.1d, 13.0d, 16.8d, 18.4d, 20.3d}, new double[]{12.0d, 13.0d, 16.8d, 18.4d, 20.3d}, new double[]{12.0d, 13.0d, 16.8d, 18.2d, 19.9d}, new double[]{12.0d, 13.0d, 16.8d, 18.4d, 20.4d}, new double[]{12.0d, 12.9d, 16.8d, 18.4d, 20.4d}};
 
 
 
-    /* renamed from: r */
+
     public static C0051c f667r;
 
 
 
-    /* renamed from: t */
+
     public HTBodyResultTwoLegs f669t;
 
 
 
-    /* renamed from: v */
+
     private int f671v;
 
 
